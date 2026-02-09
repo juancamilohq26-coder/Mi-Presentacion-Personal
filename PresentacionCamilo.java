f48dee5b4957bd4c4e2f963cc05b0ef9ca1263fc
juancamilohq26-coder <juancamilohq26@gmail.com>
2026-02-08 21:26:54 -0500

    Subi el archivo

---
+++ PresentacionCamilo.java
@@ -0,0 +1,14 @@
+public class PresentacionPersonal {
+    public static void main(String[] args) {
+
+        String nombre = "Juan Camilo Hernandez";
+        int edad = 24;
+        double estatura = 1.75;
+        String gustos = "el fútbol y los deportes";
+
+        System.out.println("Hola, mi nombre es " + nombre);
+        System.out.println("Tengo " + edad + " años");
+        System.out.println("Mi estatura es de " + estatura + " metros");
+        System.out.println("Me gusta " + gustos);
+    }
+}
