9135557122dad5a6d3a34776f9a087d820dfad94
juancamilohq26-coder <juancamilohq26@gmail.com>
2026-02-10 14:03:46 -0500

    Update PresentacionCamilo.java
    Update Presentation 2026

--- PresentacionCamilo.java
+++ PresentacionCamilo.java
@@ -1,10 +1,25 @@
-public class PresentacionPersonal {
+public class PresentacionJuanCamilo {
     public static void main(String[] args) {
 
         String nombre = "Juan Camilo Hernandez";
         int edad = 24;
         double estatura = 1.75;
         String gustos = "el fútbol y los deportes";
+
+        System.out.println("Hola, mi nombre es " + nombre);
+        System.out.println("Tengo " + edad + " años");
+        System.out.println("Mi estatura es de " + estatura + " metros");
+        System.out.println("Me gusta " + gustos);
+    }
+}
+
+public class PresentacionJuanEsteban {
+    public static void main(String[] args) {
+
+        String nombre = "Juan Esteban Ortiz";
+        int edad = 17;
+        double estatura = 1.72;
+        String gustos = "Ir al Gimnasio y Emprender";
 
         System.out.println("Hola, mi nombre es " + nombre);
         System.out.println("Tengo " + edad + " años");
